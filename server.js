389f2acc5bbbea0d7bd7586d854aa34dfe864ca4
Luke-mc <luke_mcglynn@yahoo.com>
2017-07-09 18:50:14 -1000

    got pages showing up in localhost

--- server.js
+++ server.js
@@ -8,9 +8,25 @@ const request = require('request');
 const querystring = require('querystring');
 let para = null;
 let body = [];
-
+var result = null;
 //Functions
 //
+
+const sort = (path,link)=>{
+  return {
+    path : path,
+    link : link
+  };
+};
+
+const urlChecker = (url)=>{
+  body.forEach((e)=>{
+    if(e.path === url){
+      para = e.link;
+    }
+  });
+};
+
 const fetchHtmlFiles = (fileName) => fs.readFileSync(fileName);
 
 const header = (page, res) =>{
@@ -29,7 +45,7 @@ const createElement = (elementName, elementSymbol, elementAtomicNumber, elementD
             <head>
               <meta charset="UTF-8">
               <title>The Elements - ${elementName}</title>
-              <link rel="stylesheet" href="/public/css/styles.css">
+              <link rel="stylesheet" href="/css/styles.css">
             </head>
             <body>
               <h1>${elementName}</h1>
@@ -40,95 +56,51 @@ const createElement = (elementName, elementSymbol, elementAtomicNumber, elementD
             </body>
             </html>`;
 };
-// Maybe should go into an array and have the new ones be dynamically push into the array aswell?
+// Need to make everything DRY.
 //
 const index = fetchHtmlFiles('./public/index.html');
+body.push(sort("/", index.toString()));
+body.push(sort("/index.html", index.toString()));
 const helium = fetchHtmlFiles('./public/helium.html');
+body.push(sort("/helium.html", helium.toString()));
 const hydrogen = fetchHtmlFiles('./public/hydrogen.html');
+body.push(sort("/hydrogen.html", hydrogen.toString()));
 const styles = fetchHtmlFiles('./public/css/styles.css');
+body.push(sort("/styles.html", styles.toString()));
 const four0Four = fetchHtmlFiles('./public/404.html');
+body.push(sort("/404.html", four0Four.toString()));
 
 //Create HTTP server
 //
 const server = http.createServer( (req, res) => {
-//GET start
-  if (req.method === 'GET') {
-    switch(req.url){
-      case "/":
-        para = index;
-      break;
-      case "/index.html":
-        para = index;
-      break;
-      case "/helium.html":
-        para = helium;
-      break;
-      case "/hydrogen.html":
-        para = hydrogen;
-      break;
-      case "/css/styles.css":
-        para = styles;
-      break;
-      // need to work on this :
-      // case "/elements":
-      //   code in here;
-      // break;
-      default:
-       para = four0Four;
-    }
-   header(para,res);
-  }//GET end
-
 //POST start
-  else if (req.method === 'POST') {
-    var result = null;
+if (req.method === 'POST') {
     req.on('data', (data) => {
       var chunk = data.toString();
       var parsedChunk = querystring.parse(chunk);
-      body.push(parsedChunk);
       var html = createElement(parsedChunk.elementName, parsedChunk.elementSymbol, parsedChunk.elementAtomicNumber, parsedChunk.elementDescription);
       fs.writeFile(`./public/${parsedChunk.elementName}.html`, html, (err) => {
         if (err) throw err;
-        result = fs.readFileSync(`./public/${parsedChunk.elementName}.html`);
+          result = fs.readFileSync(`./public/${parsedChunk.elementName}.html`);
+          body.push(sort(`/${parsedChunk.elementName.toLowerCase()}.html`, result.toString()));
+          console.log(body);
       });
     });
-    setTimeout(() =>{
-   //if(req.url === `/${parsedChunk.elementName}.html`){
-      console.log(req.url.split(' ')[0]);
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(result.toString());
-      res.end();
-   //}
-    },10);
+
   }//POST end
+
+//GET start
+  else if (req.method === 'GET') {
+   urlChecker(req.url);
+   header(para,res);
+  }//GET end
 
   setTimeout(() =>{
     res.end();
-    console.log(body);
+    console.log(body.length);
   },10);
 }).listen(8080);
 //Server end
 
 
 
-
-//Old POST code just using it for refererence.
-
-  //  else if (req.method === 'POST') {
-  //   let body = [];
-  //   req.on('data', (data) => {
-  //     var result = null;
-  //     var chunk = data.toString();
-  //     var parsedChunk = querystring.parse(chunk);
-  //     body.push(parsedChunk);
-  //     var html = createElement(parsedChunk.elementName, parsedChunk.elementSymbol, parsedChunk.elementAtomicNumber, parsedChunk.elementDescription);
-  //     console.log(typeof html);
-  //     fs.writeFile(`./public/${parsedChunk.elementName}.html`, html, (err) => {
-  //       if (err) throw err;
-  //       result =fs.readFileSync(`./public/${parsedChunk.elementName}.html`);
-  //     });
-  //     res.write(header(result, res));
-  //   });
-  // }
-  // res.end();
-  // }).listen(8080);
